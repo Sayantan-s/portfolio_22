1fe0f47442ae722ed997edc41970c89209030955
Sayantan Samanta <sssamanta789@gmail.com>
2023-03-17 01:38:19 +0530

    feat: fix logout dropdown

--- packages/client/src/components/shared/Layouts/Sidebar/ProfilePopper/index.tsx
+++ packages/client/src/components/shared/Layouts/Sidebar/ProfilePopper/index.tsx
@@ -2,6 +2,8 @@ import { Heading } from "@/components/ui";
 import { useWebS } from "@/context/Ws";
 import { useUser } from "@/hooks";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import * as Switch from "@radix-ui/react-switch";
+import { useState } from "react";
 
 export const ProfilePopper = () => {
   const user = useUser();
@@ -10,10 +12,12 @@ export const ProfilePopper = () => {
 
   const connectedStyles = isConnected ? "bg-emerald-300" : "bg-rose-400";
 
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div className="absolute bottom-4 w-48 left-1/2 transform -translate-x-1/2 bg-slate-50 hover:bg-slate-100 p-2 rounded-full">
       <DropdownMenu.Root>
-        <DropdownMenu.Trigger className="w-full flex items-center truncate">
+        <DropdownMenu.Trigger className="w-full flex items-center truncate focus:outline-none">
           <div className="w-10 h-10 bg-slate-200 rounded-full relative">
             <img
               src={user?.details?.profile_pic}
@@ -37,8 +41,21 @@ export const ProfilePopper = () => {
             sideOffset={12}
             className="bg-white w-60 h-full shadow-md shadow-slate-200/60 flex flex-col rounded-xl p-3 space-y-2"
           >
-            <button>Darkmode</button>{" "}
-            <DropdownMenu.Item className="bg-red-50 hover:bg-red-100 px-2 py-1 rounded-md text-red-500">
+            <label className="flex items-center justify-between px-2 py-1">
+              Darkmode
+              <Switch.Root
+                onCheckedChange={setIsChecked}
+                className="w-[42px] h-[25px] bg-sky-300 rounded-full relative"
+                id="airplane-mode"
+              >
+                <Switch.Thumb
+                  className={`w-[21px] h-[21px] shadow-md transition-transform duration-150 transform ${
+                    isChecked ? "translate-x-[2px]" : "translate-x-[19px]"
+                  } rounded-full bg-white block`}
+                />
+              </Switch.Root>
+            </label>
+            <DropdownMenu.Item className="bg-red-50 hover:bg-red-100 px-2 py-1 rounded-md text-red-500 focus:outline-none">
               Logout
             </DropdownMenu.Item>
           </DropdownMenu.Content>
